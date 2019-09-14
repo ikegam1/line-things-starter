9270cfe2a5a2a7e238f6fd578e575801622bcc87
User1 <user1@yuza1noMacBook-ea.local>
2019-09-14 16:28:58 +0900

    update

--- liff-app/liff.js
+++ liff-app/liff.js
@@ -277,7 +277,8 @@ function liffGetButtonStateCharacteristic(characteristic) {
     characteristic.startNotifications().then(() => {
         characteristic.addEventListener('characteristicvaluechanged', e => {
             const val = (new Uint8Array(e.target.value.buffer))[0];
-            if (val > 0) {
+            val = String.fromCharCode(val);
+            if (val > 0 && val < 10) {
                 // press
                 alert("receive notification : " + val);
                 //uiToggleStateButton(true);
