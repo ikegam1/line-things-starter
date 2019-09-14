723ce56c2a4ba8cf7665ea3f4ab0eff38af66614
User1 <user1@yuza1noMacBook-ea.local>
2019-09-14 20:45:53 +0900

    update

--- liff-app/liff.js
+++ liff-app/liff.js
@@ -274,6 +274,7 @@ function liffGetPSDIService(service) {
         const psdi = new Uint8Array(value.buffer)
             .reduce((output, byte) => output + ("0" + byte.toString(16)).slice(-2), "");
         //document.getElementById("device-psdi").innerText = psdi;
+        alert(psdi);
     }).catch(error => {
         uiStatusError(makeErrorMsg(error), false);
     });
